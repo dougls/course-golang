a52a2ba141633a67e834d5f3e3b58758f2a052a0
Douglas Lima <douglimadasilva@gmail.com>
2022-03-05 13:58:16 -0300

    Terceira semana de curso

--- 9 - Arrays e Slices/arrays-e-slices.go
+++ 9 - Arrays e Slices/arrays-e-slices.go
@@ -15,7 +15,34 @@ func main() {
 	array3 := [...]int{1, 2, 3, 4, 5}
 	fmt.Println(array3)
 
-	slice := []int{10, 11, 12, 13, 14, 15, 16}
+	slice := []int{10, 11, 12, 13, 14, 15, 16, 17}
 	fmt.Println(slice)
+
+	slice = append(slice, 18)
+	fmt.Println(slice)
+
+	slice2 := array2[1:3] //todo array começa no número 0, então a posição 1 = 2, nesse exemplo ele vai pegar o 1 e 2, quando chegar no 3 ele vai ignorar
+	fmt.Println(slice2)
+
+	array2[1] = "Posição Alterada"
+	fmt.Println(slice2)
+
+	// Arrays Internos
+	fmt.Println("----------")
+	slice3 := make([]float32, 10, 11)
+	fmt.Println(slice3)
+
+	slice3 = append(slice3, 5)
+	slice3 = append(slice3, 6)
+
+	fmt.Println(slice3)
+	fmt.Println(len(slice3)) //length = tamanho
+	fmt.Println(cap(slice3)) //capacity = capacidade
+
+	slice4 := make([]float32, 5)
+	fmt.Println(slice4)
+	slice4 = append(slice4, 10)
+	fmt.Println(len(slice4)) //length = tamanho
+	fmt.Println(cap(slice4)) //capacity = capacidade
 
 }
